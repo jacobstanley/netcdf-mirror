eb75efb2a12550d870b5200331626743252660af
dmh <dmh@2e3060e4-1ed8-cf1f-9cc2-9586e844a556>
2011-11-10 22:53:52 +0000

    git-svn-id: http://svn.unidata.ucar.edu/repos/netcdf/branches/minor@1632 2e3060e4-1ed8-cf1f-9cc2-9586e844a556

--- nc_test/tst_nofill.c
+++ nc_test/tst_nofill.c
@@ -108,6 +108,9 @@ create_file(char *file_name, int fill_mode, size_t* sizehintp)
     * nc__create() instead of calling nc_create() and getting the
     * block size reported by fstat */
    stat = nc__create(file_name, NC_CLOBBER, default_initialsize, sizehintp, &ncid);
+   if(stat) {
+	system("df -h");
+   }
    check_err(stat,__LINE__,__FILE__);
    stat = nc_set_fill(ncid, fill_mode, &old_fill_mode);
    check_err(stat,__LINE__,__FILE__);
